9c65b6af94ce9d726d662f2ab0b02e2dd8f7f586
shinyay <shinya.com@gmail.com>
2020-02-12 17:44:02 +0900

    Add a script to create a new cluster

---
+++ script/create-cluster.fish
@@ -0,0 +1,23 @@
+#!/usr/bin/env fish
+
+function create_cluster
+  argparse -n create-cluster 'h/help' 'c/cluster=' -- $argv
+  or return 1
+
+  if set -lq _flag_help
+    echo "create-cluster.fish -c/--cluster <CLUSTER_NAME>"
+    return
+  end
+
+  if not set -lq _flag_cluster
+    echo "create-cluster.fish -c/--cluster <CLUSTER_NAME>" & return
+  end
+
+  gcloud container clusters create $_flag_cluster \
+    --num-nodes 2 \
+    --machine-type n1-standard-1 \
+    --zone us-central1-c
+  gcloud container clusters list
+end
+
+create_cluster $argv
