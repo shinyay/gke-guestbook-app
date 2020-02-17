f264df129d23635880fb3b7255e2032261580477
shinyay <shinya.com@gmail.com>
2020-02-17 19:34:49 +0900

    Add zero-argument constructor plugin

--- guestbook/build.gradle.kts
+++ guestbook/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
 	kotlin("jvm") version "1.3.61"
 	kotlin("plugin.spring") version "1.3.61"
 	kotlin("plugin.jpa") version "1.3.61"
+	kotlin("plugin.noarg") version "1.3.61"
 }
 
 group = "io.pivotal.shinyay"
