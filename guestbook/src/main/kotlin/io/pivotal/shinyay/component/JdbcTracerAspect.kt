a09b0cf6c530e2e5f69622bc3e1adbfd4cf3ad06
shinyay <shinya.com@gmail.com>
2020-02-17 22:50:58 +0900

    Add Sleuth app

--- guestbook/src/main/kotlin/io/pivotal/shinyay/component/JdbcTracerAspect.kt
+++ guestbook/src/main/kotlin/io/pivotal/shinyay/component/JdbcTracerAspect.kt
@@ -1,11 +1,25 @@
 package io.pivotal.shinyay.component
 
+import brave.Span
 import org.aspectj.lang.annotation.Aspect
 import org.springframework.stereotype.Component
 import brave.Tracer
+import org.aspectj.lang.ProceedingJoinPoint
+import org.aspectj.lang.annotation.Around
+import org.springframework.cloud.sleuth.util.SpanNameUtil
 
 @Aspect
 @Component
 class JdbcTracerAspect(val tracer: Tracer) {
-    
+
+    @Around("execution (* org.springframework.jdbc.core.JdbcTemplate.*(..))")
+    fun traceJdbcCall(joinPoint: ProceedingJoinPoint): Any? {
+        val spanName = SpanNameUtil.toLowerHyphen(joinPoint.signature.name)
+        val span: Span = this.tracer.nextSpan().name("jdbc:/${spanName}").start()
+        try {
+            return joinPoint.proceed()
+        } finally {
+            span.finish()
+        }
+    }
 }
