4954de10021b26aa2aaaa19d5110392f19f320d9
shinyay <shinya.com@gmail.com>
2020-02-17 19:46:43 +0900

    Add message entity

---
+++ guestbook/src/main/kotlin/io/pivotal/shinyay/entity/Message.kt
@@ -0,0 +1,16 @@
+package io.pivotal.shinyay.entity
+
+import java.util.*
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+
+@Entity
+data class Message(
+        @Id
+        @GeneratedValue
+        val id: Long,
+        val username: String,
+        val message: String,
+        val timestamp: Date
+)
