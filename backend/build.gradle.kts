39940aec938d2a2975fbc5ef37cf3a34b6d8f36e
shinyay <shinya.com@gmail.com>
2020-02-25 18:16:51 +0900

    Add Build Info and Git Info

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "2.2.4.RELEASE"
 	id("io.spring.dependency-management") version "1.0.9.RELEASE"
+	id("com.gorylenko.gradle-git-properties") version "2.0.0"
 	kotlin("jvm") version "1.3.61"
 	kotlin("plugin.spring") version "1.3.61"
 }
@@ -45,4 +46,8 @@ tasks.withType<KotlinCompile> {
 		freeCompilerArgs = listOf("-Xjsr305=strict")
 		jvmTarget = "1.8"
 	}
+}
+
+springBoot {
+	buildInfo()
 }
