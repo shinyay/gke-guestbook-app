b84dce0e0018065103222a2fbec1de3ed5423764
shinyay <shinya.com@gmail.com>
2020-02-26 22:30:32 +0900

    Modify dependency for Redis Session

--- frontend/build.gradle.kts
+++ frontend/build.gradle.kts
@@ -19,9 +19,9 @@ extra["springCloudVersion"] = "Hoxton.SR1"
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
-	implementation("org.springframework.boot:spring-boot-starter-data-redis")
 	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.session:spring-session-data-redis")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
