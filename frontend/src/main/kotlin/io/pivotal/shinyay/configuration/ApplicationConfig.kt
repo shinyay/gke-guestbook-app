0638c99e0d72513f64970890a540be01c4220307
shinyay <shinya.com@gmail.com>
2020-02-26 22:36:57 +0900

    Add application configuration class template

--- frontend/src/main/kotlin/io/pivotal/shinyay/configuration/ApplicationConfig.kt
+++ frontend/src/main/kotlin/io/pivotal/shinyay/configuration/ApplicationConfig.kt
@@ -1,4 +1,9 @@
 package io.pivotal.shinyay.configuration
 
+import org.springframework.context.annotation.Configuration
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession
+
+@Configuration
+@EnableRedisHttpSession
 class ApplicationConfig {
 }
