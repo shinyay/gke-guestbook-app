2f48183f671cacdd5b5da1eeb8e8b8fcb03ed112
shinyay <shinya.com@gmail.com>
2020-02-27 19:09:28 +0900

    Add service to call guestbook-service with REST

--- frontend/src/main/kotlin/io/pivotal/shinyay/service/GuestbookService.kt
+++ frontend/src/main/kotlin/io/pivotal/shinyay/service/GuestbookService.kt
@@ -1,4 +1,19 @@
 package io.pivotal.shinyay.service
 
-class GuestbookService {
+import org.springframework.stereotype.Service
+import org.springframework.web.client.RestTemplate
+import org.springframework.web.client.getForObject
+import org.springframework.web.client.postForObject
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
+
+@Service
+class GuestbookService(val restTemplate: RestTemplate, val endPoint: String) {
+
+    val dateFormat: DateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ")
+
+    fun add(username: String, message: String): Map<String, String> {
+        return restTemplate.postForObject(endPoint, mapOf("username" to username, "message" to message, "timestamp" to dateFormat.format(Date())))
+    }
 }
